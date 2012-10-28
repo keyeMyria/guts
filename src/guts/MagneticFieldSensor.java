fbc7964f1254b2d3076525f982eece3856ebfce0
Cedric Ohle <ohle.cedric@googlemail.com>
2012-10-28 12:29:59 +0100

    created magneticfieldsensor class

---
+++ src/guts/MagneticFieldSensor.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guts;
+
+/**
+ *
+ * @author Cedric Ohle
+ */
+public class MagneticFieldSensor {
+    private int address;
+    
+    public float fetchAngleToMagneticNorth(){
+        //todo: needs driver access and implementation
+        return 0;
+    }
+}
