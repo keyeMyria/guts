d152fbcd6d40f30261ec36375e6d0aca93cd51a1
Cedric Ohle <ohle.cedric@googlemail.com>
2012-10-28 11:48:18 +0100

    created gyro class

---
+++ src/guts/Gyroscope.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guts;
+
+/**
+ *
+ * @author Cedric Ohle
+ */
+public class Gyroscope {
+    private int address;
+    
+    public Axis fetchPosition(){
+        //todo: needs driver access and implementation
+        return null;
+    }
+}
