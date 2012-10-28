75dc81eb19aca9ea1c464eabc237acc93bc7b1c4
Cedric Ohle <ohle.cedric@googlemail.com>
2012-10-28 12:56:50 +0100

    Added javadoc

--- src/guts/Axis.java
+++ src/guts/Axis.java
@@ -1,11 +1,9 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package guts;
 
 /**
- *
+ * This class represents an axis object to describe the postion of a object in
+ * the three dimensional space.
  * @author Cedric Ohle
  */
 public class Axis {
@@ -13,26 +11,50 @@ public class Axis {
     private float yawn;
     private float roll;
     
+    /**
+     * Sets the pitchangle
+     * @param pitch as float
+     */
     public void setPitch(float pitch){
         this.pitch = pitch;
     }
     
+    /**
+     * Sets the yawnangle
+     * @param yawn as float
+     */
     public void setYawn(float yawn){
         this.yawn = yawn;
     }
     
+    /**
+     * Sets the rollangle
+     * @param roll as float
+     */
     public void setRoll(float roll){
         this.roll = roll;
     }
     
+    /**
+     * Returns the pitchangle
+     * @return the pitchangle as float
+     */
     public float getPitch(){
         return this.pitch;
     }
     
+    /**
+     * Returns the yawnangle
+     * @return the yawnangle as float
+     */
     public float getYawn(){
         return this.yawn;
     }
     
+    /**
+     * Returns the rollangle
+     * @return the rollangle as float
+     */
     public float getRoll(){
         return this.roll;
     }
