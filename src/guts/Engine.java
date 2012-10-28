20ed9bee57c5caa1a4015909e55a9f43fb4c9687
Cedric Ohle <ohle.cedric@googlemail.com>
2012-10-28 12:08:20 +0100

    created engine-class

---
+++ src/guts/Engine.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guts;
+
+/**
+ *
+ * @author Cedric Ohle
+ */
+class Engine {
+    private int address;
+    
+    public void moveToAngle(float angle){
+        
+    }
+    
+    public float fetchAngle(){
+        //todo: needs driver access and implementation
+        return 0;
+    }
+    
+    private void move(float move){
+        
+    }
+}
