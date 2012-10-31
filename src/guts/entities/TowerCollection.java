2b75c2b6e05f0df549ee49934e7a45c42c296f17
Cedric Ohle <ohle.cedric@googlemail.com>
2012-10-31 09:39:08 +0100

    added comments

--- src/guts/entities/TowerCollection.java
+++ src/guts/entities/TowerCollection.java
@@ -1,24 +1,30 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package guts.entities;
 
 import java.util.Iterator;
 import java.util.LinkedList;
 
 /**
- *
+ * This Class holds a list of towers.
  * @author Cedric Ohle
  */
 public class TowerCollection implements Iterable<Tower> {
 
     private LinkedList<Tower> list = new LinkedList<Tower>();
     
+    /**
+     * Allows to add a tower.
+     * @param tower
+     */
     public void add(Tower tower){
         list.add(tower);
     }
     
+    /**
+     * Finds a tower in the list by ID
+     * @param ID
+     * @return towerobject or null if not found
+     */
     public Tower findByID(int ID){
         for (Tower t : list){
             if (t.getID() == ID){
@@ -29,11 +35,19 @@ public class TowerCollection implements Iterable<Tower> {
     }
     
     
+    /**
+     * Iterator for list
+     * @return iterator
+     */
     @Override
     public Iterator<Tower> iterator() {
         return list.iterator();
     }
     
+    /**
+     * Returns if the list is empty
+     * @return ture if empty else false
+     */
     public boolean isEmpty(){
         return list.isEmpty();
     }
