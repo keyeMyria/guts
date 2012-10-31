2526556f6a375731c68182b14a22acd0b7cab7eb
Cedric Ohle <ohle.cedric@googlemail.com>
2012-10-31 12:32:02 +0100

    Included and added simulation to sensors

--- src/guts/sensors/GPS.java
+++ src/guts/sensors/GPS.java
@@ -2,7 +2,10 @@
 
 package guts.sensors;
 
+import guts.Config;
 import guts.entities.Location;
+import guts.sim.SimGPS;
+import java.util.Date;
 
 /**
  * This class provides access to the locationdata from a GPS. It handles the
@@ -11,13 +14,32 @@ import guts.entities.Location;
  */
 public class GPS {
     private int address;
+    private SimGPS simGPS;
+    
+    public GPS(int address){
+        this.address = address;
+    }
+    
+    public GPS(){
+        this.simGPS = new SimGPS();
+    }
+    
     
     /**
      * This function returns the current location as a location object.
      * @return location as a location object
      */
     public Location fetchLocation(){
-        // Todo: Needs driver access and implementation
-        return null;
+        if (Config.SIMULATIONENABLED == true){
+            Location l = new Location();
+            l.setTimestamp(new Date(System.currentTimeMillis()));
+            l.setLongitude(this.simGPS.getLongitude());
+            l.setLatitude(this.simGPS.getLatitude());
+            return l;
+        }
+        else{
+            // Implement real hardware access
+            return null;
+        }
     }
 }
--- src/guts/sensors/MagneticFieldSensor.java
+++ src/guts/sensors/MagneticFieldSensor.java
@@ -1,6 +1,9 @@
 
 package guts.sensors;
 
+import guts.Config;
+import guts.sim.SimMagneticFieldSensor;
+
 /**
  * This class represents an magneticfieldsensor to allow accessing and
  * interpretating the returned values of the hardwaresensor.
@@ -8,6 +11,15 @@ package guts.sensors;
  */
 public class MagneticFieldSensor {
     private int address;
+    private SimMagneticFieldSensor simMagneticFieldSensor;
+    
+    public MagneticFieldSensor(){
+        this.simMagneticFieldSensor = new SimMagneticFieldSensor();
+    }
+    
+    public MagneticFieldSensor(int address){
+        this.address = address;
+    }
     
     
     /**
@@ -15,7 +27,11 @@ public class MagneticFieldSensor {
      * @return Angle to magnetic north as float
      */
     public float fetchAngleToMagneticNorth(){
-        //todo: needs driver access and implementation
-        return 0;
+        if (Config.SIMULATIONENABLED == true){
+            return this.simMagneticFieldSensor.getAngleToMagneticNorth();
+        } else{
+            // Implement real hardware access
+            return 0;
+        }
     }
 }
