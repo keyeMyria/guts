a0d709b6c3e7595cdf663a81553628be188552ca
Cedric Ohle <ohle.cedric@googlemail.com>
2012-10-31 12:04:01 +0100

    created config

---
+++ src/guts/Config.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guts;
+
+/**
+ *
+ * @author Cedric Ohle
+ */
+public class Config {
+    public static final boolean SIMULATIONENABLED = true;
+
+}
