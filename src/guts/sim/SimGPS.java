ac0080aa0fdacd6b6d054ddb0fd7c80eeefb93ad
Cedric Ohle <ohle.cedric@googlemail.com>
2012-11-03 14:32:57 +0100

    implemented simGPS

--- src/guts/sim/SimGPS.java
+++ src/guts/sim/SimGPS.java
@@ -4,18 +4,40 @@
  */
 package guts.sim;
 
+import guts.Config;
+import guts.entities.Location;
+
 /**
  *
  * @author Cedric Ohle
  */
 public class SimGPS {
-
-    public float getLongitude() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    
+    private Location newLocation;
+    
+    public void setLocation(Location startLocation){
+        this.newLocation = startLocation;
+    }
+    
+    public void generateNewLocation(){
+        //Todo: Max 180 abfangen, bei Long und lat
+        
+        double angel = SimMagneticFieldSensor.getCurrentAngel();
+        
+        double longitudedelta = (Math.random() * 300+1)/(160000000/ Config.REFRESHRATE);
+        
+        this.newLocation.setLongitude(this.newLocation.getLongitude() + longitudedelta);
+        
+        this.newLocation.setLatitude(this.newLocation.getLatitude() + Math.sin(angel));
+        
     }
 
-    public float getLatitude() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public double getLongitude() {
+        return this.newLocation.getLongitude();
+    }
+
+    public double getLatitude() {
+        return this.newLocation.getLatitude();
     }
     
 }
