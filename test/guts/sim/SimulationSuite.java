03137e96f3dda4fce0e65d6e32ccba3d4e460f1d
Patrick Selge <pselge@cklio.com>
2012-11-13 14:15:33 +0100

    added a new simulation suite for testing and added the sim utilities to the game!

---
+++ test/guts/sim/SimulationSuite.java
@@ -0,0 +1,38 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guts.sim;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ *
+ * @author patrick
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({guts.sim.SimMagneticFieldSensorTest.class})
+public class SimulationSuite {
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+    
+}
