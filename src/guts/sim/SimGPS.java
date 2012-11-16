e714ff0aae888fbc0e305a9278ae0cd0ed07cd8e
Cedric Ohle <ohle.cedric@googlemail.com>
2012-11-16 16:31:23 +0100

    removed todos

--- src/guts/sim/SimGPS.java
+++ src/guts/sim/SimGPS.java
@@ -39,23 +39,17 @@ public class SimGPS {
         double longitudedelta = ((Math.random() * FACTOR+1)/(DIVIDER/ Config.REFRESHRATE))*speed;
         
         if(angel == 0) {
-                //TODO x=x y=y+dy
                 newLongitude = this.location.getLongitude();
                 newLatitude = this.location.getLatitude() + (Math.random() * FACTOR+1)/(DIVIDER/ Config.REFRESHRATE);
         } else if(angel == 90) {
-                //TODO x=dx+x y=y
                 newLongitude = this.location.getLongitude() + (Math.random() * FACTOR+1)/(DIVIDER/ Config.REFRESHRATE);
                 newLatitude = this.location.getLatitude() ;
         } else if(angel == 180) {
-                //TODO x=x y=y-dy
                 newLongitude = this.location.getLongitude();
-                newLatitude = this.location.getLatitude() - (Math.random() * FACTOR+1)/(DIVIDER/ Config.REFRESHRATE);
-                
+                newLatitude = this.location.getLatitude() - (Math.random() * FACTOR+1)/(DIVIDER/ Config.REFRESHRATE);           
         } else if(angel == 270) {
-                //TODO x=x-dx y=y
                 newLongitude = this.location.getLongitude() - (Math.random() * FACTOR+1)/(DIVIDER/ Config.REFRESHRATE);
-                newLatitude = this.location.getLatitude();
-                
+                newLatitude = this.location.getLatitude();         
         } else if(angel > 0 && angel < 90 ){
             newLongitude = this.location.getLongitude() + longitudedelta;
             newLatitude = this.location.getLatitude() + (Math.tan(Math.toRadians(90-angel))*longitudedelta);
@@ -65,7 +59,6 @@ public class SimGPS {
         } else if(angel > 180 && angel < 270){
             newLongitude = this.location.getLongitude() - longitudedelta;
             newLatitude = this.location.getLatitude() - (Math.tan(Math.toRadians(270-angel))*longitudedelta);
-            // /(DIVIDER/(Config.REFRESHRATE*1000000))
         } else {
             newLongitude = this.location.getLongitude() - longitudedelta;
             newLatitude = this.location.getLatitude() + (Math.tan(Math.toRadians(angel%90))*longitudedelta);
