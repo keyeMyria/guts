921933badebad36822ca565e02868904b97b1703
Cedric Ohle <ohle.cedric@googlemail.com>
2013-01-09 15:14:12 +0100

    remove comments and add observer

--- src/guts/sim/Simulation.java
+++ src/guts/sim/Simulation.java
@@ -10,36 +10,6 @@ import guts.GUTSEntry;
  */
 public class Simulation implements Runnable{
     
-    /* todo:
-     * Gebraucht werden: Compass, GPS, Gyro
-     * Werden in dieser Reihenfolge berechnet
-     * 
-     * Vorbereitung
-     * 1. Objekte erzeugen
-     * 2. Observerbekanntmachung
-     * 
-     * Initiale Berechnung
-     * 1.       Compass Richtung festlegen und speichern
-     * 1.1          Compass benachrichtigt GPS
-     * 2.       GPS holt Daten und ermittelt neue Position ausgehend vom Startwert
-     * 2.1          Kommt es zum überlauf-> Rückmeldung an Compass
-     * 2.2          Compass speichert neue zukünftige Richtung
-     * 3.       Gyro ermittelt Startlage
-     * 
-     * weiterführende berechnung
-     * 1.       Compass prüft ob neue Richtung gesetzt
-     * 1.1          Compass ermittelt Drehung
-     * 1.2          Compass benachrichtigt GPS
-     * 2.       GPS holt die Daten und ermittelt neue Position ausgehend von
-     *          letzter Position
-     * 2.1          Überlauf prüfen -> Rückmeldung an Compass
-     * 2.2          Compass speicher neue ausrichtung
-     * 3.       Gyro ermittelt neue Lage ausgehend von letzter Lage
-     * 
-     * 
-     * 
-     */
-    
     SimMagneticFieldSensor simCompass;
     SimGPS simGPS;
     SimGyroscope simGyro;
@@ -48,6 +18,7 @@ public class Simulation implements Runnable{
         this.simCompass=SimMagneticFieldSensor.getInstance();
         this.simGPS=SimGPS.getInstance();
         this.simGyro=SimGyroscope.getInstance();
+        this.simGPS.addObserver(simCompass);
     }
 
     @Override
