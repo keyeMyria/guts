1cc09774defc0fabf039c464f52fd6959b43f87d
Patrick Selge <pselge@cklio.com>
2013-01-14 09:34:41 +0100

    refactored antennaCorrectionCalculator and build some javadocs

--- src/guts/calculators/AntennaCorrectionCalculator.java
+++ src/guts/calculators/AntennaCorrectionCalculator.java
@@ -6,6 +6,7 @@ package guts.calculators;
 
 import guts.entities.Axis;
 import guts.entities.Location;
+import osmViewer.data.Tower;
 
 /**
  * This class fetches the values of the current position 
@@ -15,79 +16,73 @@ import guts.entities.Location;
  */
 public class AntennaCorrectionCalculator {
     
+    private Location currentLocation;
+    private Tower activeTower;
+    private double currentAngle;
+    
+    /**
+     * 
+     * @param currentLocation
+     * @param currentAxis
+     * @param currentAngle
+     * @param activeTower
+     * @return new Axis Object
+     */
     public Axis calculateCorrection(Location currentLocation, Axis currentAxis, 
-            double currentAngle, Location activeTowerLocation){ 
+        double currentAngle, Tower activeTower){
+        // Grenzwerte festlegen
+        if (currentAngle < 0 || currentAngle >= 360){
+            throw new IllegalArgumentException("currentAngle must be between 0 and 359");
+        }
 
-    
-    double i, newAngle, deltax, deltay, pitch, newPitch, roll, newRoll;
-
-    calculateAngel(currentLocation, currentAxis, currentAngle, activeTowerLocation);
-    return calculateAxis(currentLocation, currentAxis, currentAngle);
-
-    }
-
-public double calculateAngel(Location currentLocation, Axis currentAxis, 
-            double currentAngle, Location activeTowerLocation){
-  
-    if (currentAngle < 0 || currentAngle >= 360){
-        throw new IllegalArgumentException("currentAngle must be between 0 and 359");
-    }
-    
-  double i, newAngle, deltax, deltay;
-  
-  // deltax und deltay berechnet sich aus der Differenz vom Standpunkt des 
-  // gewählten Towers und vom Standpunkt der aktuellen Position des Wagens
-  deltax = activeTowerLocation.getLongitude() - currentLocation.getLongitude();
-  deltay = activeTowerLocation.getLatitude() - currentLocation.getLatitude();
+        // Set attributes
+        this.currentLocation = currentLocation;
+        this.currentAngle = currentAngle;
+        this.activeTower = activeTower;
         
-  //wenn kein deltay, dann prüfen ob deltax im pos. bereich liegt (genau auf der achse) 
-  // wenn ja dann newangle 0
-  // wenn nicht dann zurückgegebener Winkel dementsprechend,damit es im pos bereich liegt
-       if (deltay == 0) {
-           if(deltax > 0){
-               newAngle=90;
-           }
-           else{
-               newAngle=270;
-           }
-       }          
-       //genau wie oben, falls kein deltax, deltay überprüfen (was wieder genau auf der achse liegt, 
-       //im welchem bereich das liegt 
-       else if (deltax == 0) {
-          if(deltay > 0){
-              newAngle=0;
-          }
-          else {
-              newAngle=180;
-          }
-       }
-      
-       //wenn keines dieser fälle eintritt also nicht genau auf der achse und nicht im neg bereich
-       // dann werden diese werte genommen und der arcus tangens wird berechnet
-       else{
-          i=deltay/deltax;
-          newAngle = Math.atan(Math.toRadians(i));
+        return new Axis(-currentAxis.getPitch(), calculateAngel(), -currentAxis.getRoll());
+    }
+
+    /**
+     * 
+     * @return new angle as double
+     */
+    private double calculateAngel(){
+        double i, newAngle, deltax, deltay;
+
+        // deltax und deltay berechnet sich aus der Differenz vom Standpunkt des 
+        // gewählten Towers und vom Standpunkt der aktuellen Position des Wagens
+        deltax = activeTower.getLongitude() - currentLocation.getLongitude();
+        deltay = activeTower.getLatitude() - currentLocation.getLatitude();
+
+        // Falls die Position des Towers und des Fahrzeuges die selbe ist, soll die Antenne
+        // nach vorne zeigen
+        if ((deltax == 0) && (deltay == 0)) {
+            return 0;
         }
         
-       //der endgültige newAngle ergibt sich dann aus der Differenz aus dem gesamten Winkelbereich
-       // und der Summe des momentanen Winkels der Antennenausrichtung und des eben berechneten 
-       // Winkels newAngle. Damit wir korrekte Werte haben, die im gültigen Bereich (360) liegen
-       // wird der modulo gebildet
-       newAngle = (360 - currentAngle + newAngle) % 360;
+        // wenn kein deltay, dann prüfen ob deltax im pos. bereich liegt (genau auf der achse) 
+        // wenn ja dann newangle 0
+        // wenn nicht dann zurückgegebener Winkel dementsprechend,damit es im pos bereich liegt
+        if (deltay == 0) {
+            newAngle = (deltax > 0) ? 90 : 270;
+        }          
+        // genau wie oben, falls kein deltax, deltay überprüfen (was wieder genau auf der achse liegt, 
+        // im welchem bereich das liegt 
+        else if (deltax == 0) {
+           newAngle = (deltay > 0) ? 0 : 180;
+        }
+        // wenn keines dieser fälle eintritt also nicht genau auf der achse und nicht im neg bereich
+        // dann werden diese werte genommen und der arcus tangens wird berechnet
+        else{
+           i=deltay/deltax;
+           newAngle = Math.atan(Math.toRadians(i));
+        }
 
-       return newAngle;
-}
-
-    public Axis calculateAxis(Location currentLocation, Axis currentAxis, double newAngle){
-        
-       double pitch, newPitch, roll, newRoll;
-
-       
-       roll=currentAxis.getRoll();
-       newRoll=roll * (-1);
-       pitch=currentAxis.getPitch();
-       newPitch=pitch * (-1);
-       
-       return new Axis(newPitch,newAngle,newRoll);
+        // der endgültige newAngle ergibt sich dann aus der Differenz aus dem gesamten Winkelbereich
+        // und der Summe des momentanen Winkels der Antennenausrichtung und des eben berechneten 
+        // Winkels newAngle. Damit wir korrekte Werte haben, die im gültigen Bereich (360) liegen
+        // wird der modulo gebildet
+        return (360 - currentAngle + newAngle) % 360;
     }
 }
--- test/guts/calculators/AntennaCorrectionCalculatorTest.java
+++ test/guts/calculators/AntennaCorrectionCalculatorTest.java
@@ -6,9 +6,12 @@ package guts.calculators;
 
 import guts.entities.Axis;
 import guts.entities.Location;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
+import static org.junit.Assert.*;
+import osmViewer.data.Tower;
 
 /**
  *
@@ -17,19 +20,41 @@ import org.junit.rules.ExpectedException;
 
 public class AntennaCorrectionCalculatorTest {
     
+     AntennaCorrectionCalculator acc;
+    
+     @Before
+     public void Before() {
+         acc = new AntennaCorrectionCalculator();
+     }
+    
      @Rule
      public ExpectedException exception = ExpectedException.none();
 
+     @Test
+     public void testCurrentAngleInBounds() 
+     {
+        acc.calculateCorrection(new Location(0.0,0.0),new Axis(),25, new Tower(0.0,0.0, "Test"));
+     }
+     
      // testet, ob currentAngle innerhalb 0 und 359 liegt 
      // da die Werte, die außerhalb liegen, ungültig sind
      @Test
      public void testCurrentAngleOutOfBounds() 
      {
-    AntennaCorrectionCalculator testClass = new AntennaCorrectionCalculator();
-    exception.expect(IllegalArgumentException.class);
-    testClass.calculateAngel(new Location(0.0,0.0),new Axis(),-1, new Location(0.0,0.0));
-
-    testClass.calculateAngel(new Location(0.0,0.0),new Axis(),360, new Location(0.0,0.0));
+        exception.expect(IllegalArgumentException.class);
+        
+        acc.calculateCorrection(new Location(0.0,0.0),new Axis(),-1, new Tower(0.0,0.0, "Test"));
+        acc.calculateCorrection(new Location(0.0,0.0),new Axis(),360, new Tower(0.0,0.0, "Test"));
+     }
+     
+     @Test
+     public void testInvertAngleAndZeroDelta()
+     {
+        Axis actAxis = acc.calculateCorrection(new Location(0.0,0.0), new Axis(20.0,0.0,20.0), 0, new Tower(0.0, 0.0, "Test"));
+        Axis reqAxis = new Axis(-20.0,0.0,-20.0);
+        
+        // Gramm fragen, warum assertEquals nur mit der toString() Methode funktioniert
+        assertEquals(reqAxis.toString(), actAxis.toString());
      }
 }
 
